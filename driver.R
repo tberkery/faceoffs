2b23043b48be1195847027d137d668c45d5309ca
tberkery <78045727+tberkery@users.noreply.github.com>
2022-10-27 00:36:57 -0400

    Context stats complete. Now have 'nhl_player_seasons.csv' which contains every player season from 2007-2022 (as in ending with the 2021-2022 season). This file contains basically every stat out there to form a player profile (except Corey Sznajder microstats). NAs entirely removed (overwrote NAs in draft-related columns to read -1 to indicate a player went undrafted). Data is conditioned and should be ripe for ML models!

--- driver.R
+++ driver.R
@@ -16,15 +16,20 @@ get_player_data = function() {
   eh_box = read_eh_box()
   eh_rel = read_eh_relative() %>% select(-c(Player, `API ID`, Team, Position, Shoots, Birthday, Age, `Draft Yr`, `Draft Rd`, `Draft Ov`, GP, TOI))
   
-  skaters = eh_box %>% inner_join(eh_rel, by = c('EH_ID', 'Season'))
+  skaters = eh_box %>% inner_join(eh_rel, by = c('EH_ID', 'Season'), suffix = c('', '_relative'))
   
   eh_on_ice = read_eh_on_ice() %>% select(-c(Player, `API ID`, Team, Position, Shoots, Birthday, Age, `Draft Yr`, `Draft Rd`, `Draft Ov`, GP, TOI))
   
-  skaters = skaters %>% inner_join(eh_on_ice, by = c('EH_ID', 'Season'))
+  skaters = skaters %>% inner_join(eh_on_ice, by = c('EH_ID', 'Season'), suffix = c('', '_on_ice'))
   
   eh_zones = read_eh_zones() %>% select(-c(Player, `API ID`, Team, Position, Shoots, Birthday, Age, `Draft Yr`, `Draft Rd`, `Draft Ov`, GP, TOI))
   
-  skaters = skaters %>% inner_join(eh_zones, by = c('EH_ID', 'Season'))
+  skaters = skaters %>% inner_join(eh_zones, by = c('EH_ID', 'Season'), suffix = c('', '_zones'))
+  
+  eh_gar_skaters = read_eh_gar_skaters() %>% select(-c(Player, `API ID`, Team, Position, Shoots, Birthday, Age, `Draft Yr`, `Draft Rd`, `Draft Ov`, GP))
+  
+  skaters = skaters %>% inner_join(eh_gar_skaters, by = c('EH_ID', 'Season'), suffix = c('', '_gar'))
+  
   return(skaters)
 }
 
@@ -45,11 +50,11 @@ compute_goaltending_by_team_season = function() {
     distinct(Team, Season, .keep_all = TRUE)
   
   team_season_goaltending = team_season_goaltending %>%
-    inner_join(team_season_goalie_gar, by = c('Team', 'Season'))
+    inner_join(team_season_goalie_gar, by = c('Team', 'Season'), suffix = c('', '_gar'))
   return(team_season_goaltending)
 }
 
-connnect_skaters_to_team_goaltending = function() {
+connect_skaters_to_team_goaltending = function() {
   skaters = get_player_data()
   team_season_goaltending = compute_goaltending_by_team_season()
   skaters_with_goaltending = skaters %>%
@@ -78,5 +83,104 @@ connect_skaters_and_goaltending_to_team_performance = function() {
   eh_team_summary = compute_team_performance_by_season()
   skaters_with_goaltending_and_teams = skaters_with_goaltending %>%
     inner_join(eh_team_summary, by = c('Team', 'Season'), suffix = c('', '_team'))
+  skaters_with_goaltending_and_teams = skaters_with_goaltending_and_teams %>%
+    select(-c(Birthday, `FO±`))
+  skaters_with_goaltending_and_teams = skaters_with_goaltending_and_teams %>%
+    rename_cols()
+  skaters_with_goaltending_and_teams = skaters_with_goaltending_and_teams %>%
+    mutate(Draft_Yr = replace_na(Draft_Yr, -1)) %>%
+    mutate(Draft_Rd = replace_na(Draft_Rd, -1)) %>%
+    mutate(Draft_Ov = replace_na(Draft_Ov, -1))
+  skaters_with_goaltending_and_teams %>% na.omit() %>% write_csv("nhl_player_seasons.csv")
   return(skaters_with_goaltending_and_teams)
 }
+
+rename_cols = function(skaters_with_goaltending_and_teams) {
+  skaters_with_goaltending_and_teams = skaters_with_goaltending_and_teams %>%
+    rename(Draft_Yr = `Draft Yr`,
+           Draft_Rd = `Draft Rd`,
+           Draft_Ov = `Draft Ov`,
+           FSh_Percent = `FSh%`,
+           xFSh_Percent = `xFSh%`,
+           iPEN_Plus_Minus = `iPEN±`,
+           RelTM_G_Plus_Minus_per_60 = `RelTM G±/60`,
+           RelTM_S_Plus_Minus_per_60 = `RelTM S±/60`,
+           RelTM_F_Plus_Minus_per_60 = `RelTM F±/60`,
+           RelTM_C_Plus_Minus_per_60 = `RelTM C±/60`,
+           RelTM_xG_Plus_Minus_per_60 = `RelTM xG±/60`,
+           RelTM_GF_per_60 = `RelTM GF/60`,
+           RelTM_GA_per_60 = `RelTM GA/60`,
+           RelTM_SF_per_60 = `RelTM SF/60`,
+           RelTM_SA_per_60 = `RelTM SA/60`,
+           RelTM_FF_per_60 = `RelTM FF/60`,
+           RelTM_FA_per_60 = `RelTM FA/60`,
+           RelTM_CF_per_60 = `RelTM CF/60`,
+           RelTM_CA_per_60 = `RelTM CA/60`,
+           RelTM_xGF_per_60 = `RelTM xGF/60`,
+           RelTM_xGA_per_60 = `RelTM xGA/60`,
+           GF_Percent_on_ice = `GF%`,
+           SF_Percent_on_ice = `SF%`,
+           FF_Percent_on_ice = `FF%`,
+           CF_Percent_on_ice = `CF%`,
+           xGF_Percent_on_ice = `xGF%`,
+           GF_per_60_on_ice = `GF/60`,
+           GA_per_60_on_ice = `GA/60`,
+           SF_per_60_on_ice = `SF/60`,
+           SA_per_60_on_ice = `SA/60`,
+           FF_per_60_on_ice = `FF/60`,
+           FA_per_60_on_ice = `FA/60`,
+           CF_per_60_on_ice = `CF/60`,
+           CA_per_60_on_ice = `CA/60`,
+           xGF_per_60_on_ice = `xGF/60`,
+           xGA_per_60_on_ice = `xGA/60`,
+           G_Plus_Minus_per_60_on_ice = `G±/60`,
+           S_Plus_Minus_per_60_on_ice = `S±/60`,
+           F_Plus_Minus_per_60_on_ice = `F±/60`,
+           C_Plus_Minus_per_60_on_ice = `C±/60`,
+           xG_Plus_Minus_per_60_on_ice = `xG±/60`,
+           Sh_Percent_on_ice = `Sh%_on_ice`,
+           Sv_Percent_on_ice = `Sv%`,
+           TOI_per_GP = `TOI/GP`,
+           TOI_Percent = `TOI%`,
+           OZS_Percent = `OZS%`,
+           NZS_Percent = `NZS%`,
+           DZS_Percent = `DZS%`,
+           OTF_Percent = `OTF%`,
+           OZF_Percent = `OZF%`,
+           NZF_Percent = `NZF%`,
+           DZF_Percent = `DZF%`,
+           Ice_Percent = `Ice%`,
+           Sv_Percent_goaltending = `Sv%_goaltending`,
+           FSv_Percent = `FSv%`,
+           xFSv_Percent = `xFSv%`,
+           dFSv_Percent = `dFSv%`,
+           xGF_Percent_team = `xGF%_team`,
+           GF_per_60_team = `GF/60_team`,
+           GA_per_60_team = `GA/60_team`,
+           SF_per_60_team = `SF/60_team`,
+           SA_per_60_team = `SA/60_team`,
+           FF_per_60_team = `FF/60_team`,
+           FA_per_60_team = `FA/60_team`,
+           CF_per_60_team = `CF/60_team`,
+           CA_per_60_team = `CA/60_team`,
+           xGF_per_60_team = `xGF/60_team`,
+           xGA_per_60_team = `xGA/60_team`,
+           G_Plus_Minus_per_60_team = `G±/60_team`,
+           S_Plus_Minus_per_60_team = `S±/60_team`,
+           F_Plus_Minus_per_60_team = `F±/60_team`,
+           C_Plus_Minus_per_60_team = `C±/60_team`,
+           xG_Plus_Minus_per_60_team = `xG±/60_team`,
+           Sh_Percent_Adjusted_team = `Sh%_adjusted`,
+           Sv_Percent_Adjusted_team = `Sv%_adjusted`,
+           W_team = W,
+           L_team = L,
+           OL_team = OL,
+           Points_Percent_team = `Points%`,
+           GF_team = GF,
+           G_Plus_Minus_team = `G±`,
+           Sh_Percent_team = `Sh%_team`,
+           Sv_Percent_team = `Sv%_team`
+    )
+  return(skaters_with_goaltending_and_teams)
+}
+
