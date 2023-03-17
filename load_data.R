c71b3e83b6c1d43b883990d8724c7d5939139f55
tberkery <78045727+tberkery@users.noreply.github.com>
2023-03-17 01:37:08 -0400

    I think role-encoding is now working bu tmore auditing needed. Only potential issue relates to NAs, but I believe issue was a human error where I was joining to the wrong number of years.

--- load_data.R
+++ load_data.R
@@ -205,6 +205,8 @@ filter_games = function(pbp_with_sznajder) {
   #   arrange(game_date, home_team, away_team, game_seconds)
   print(colnames(pbp_with_zone_changes))
   print(nrow(pbp_with_zone_changes))
+  zone_entries %>% write_csv("zone_entries_current.csv")
+  zone_exits %>% write_csv("zone_exits_current.csv")
 }
 
 reset = function() {
