5a18679bef736714b99bc795e7eba2a8a4b7c30f
tberkery <78045727+tberkery@users.noreply.github.com>
2023-04-06 18:20:43 -0400

    Working to streamline running process for our code. See run.R.

--- run.R
+++ run.R
@@ -1,15 +1,20 @@
-source("load_data.R")
-source("load_data_20-22.R")
-source("Join_Entries.R")
-source("xgBoost_modeling.R")
-run = function() {
-  pbp_17 = load_eh_pbp(2017, 2018)
-  pbp_20 = load_eh_pbp(2020, 2021)
-  pbp_21 = load_eh_pbp(2021, 2022)
-  pbp = pbp_17 %>% rbind(pbp_20) %>% rbind(pbp_21)
-  load_sznajder_game_reports(pbp, 2017, 2022)
-  zone_entries = read_csv("zone_entries_current.csv")
-  zone_exits = read_csv("zone_exits_current.csv")
-  big_join = join_entries(data, zone_entries, zone_exits)
-  faceoffs_full = condition(big_join)
+
+assemble_stats = function() {
+  source("driver.R")
+  mega_dict = connect_skaters_and_goaltending_to_team_performance()
+}
+
+load_sznajder = function() {
+  source("load_data.R")
+  load_season(2017, 2019) # note that this does the 2017-2018 and 2018-2019 seasons (i.e. boundaries are inclusive, exclusive)
+  zone_entries_17_18_19 = read_csv("zone_entries_intermediate.csv")
+  zone_exits_17_18_19 = read_csv("zone_exits_intermediate.csv")
+  source("load_data_20-22.R")
+  load_season(2020, 2022) # same note... this is 2020-2021 and 2021-2022.
+  zone_entries_20_21_22 = read_csv("zone_entries_intermediate.csv")
+  zone_exits_20_21_22 = read_csv("zone_exits_intermediate.csv")
+  all_zone_entries = rbind(zone_entries_17_18_19, zone_entries_20_21_22)
+  all_zone_exits = rbind(zone_exits_17_18_19, zone_exits_20_21_22)
+  source("Join_Entries.R")
+  big_join = join_entries(2017, 2022, all_zone_entries, all_zone_exits)
 }
