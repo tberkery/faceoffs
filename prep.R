a17f6ad1b34613afdd3a77e654b34702a10ce7d5
tberkery <78045727+tberkery@users.noreply.github.com>
2023-04-13 18:32:18 -0400

    updated prep.R to prep data files for models of interest

--- prep.R
+++ prep.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-data = read_csv("dataset_300k_rel_cols_no_na.csv") # used to be temporary_training_set.csv
+data = dataset
 data = data %>%
   mutate(net_xg = winner_xg + loser_xg) %>%
   mutate(faceoff_type = case_when(
@@ -9,18 +9,197 @@ data = data %>%
     event_zone != 'Neu' &  ((last_faceoff_winner == home_team & home_zone == 'Def') | (last_faceoff_winner != home_team & home_zone == 'Off')) ~ 'Def zone won by Def team',
     event_zone != 'Neu' &  ((last_faceoff_winner != home_team & home_zone == 'Def') | (last_faceoff_winner == home_team & home_zone == 'Off')) ~ 'Off zone won by Off team',
     TRUE ~ 'other'
-  ))
-data = data %>%
-  #select(faceoff_type, net_xg, Win_F1, Win_F2, Win_F3, Win_D1, Win_D2, Win_G1, Lose_F1, Lose_F2, Lose_F3, Lose_D1, Lose_D2, Lose_G1,
-  #       starts_with('G_') & !contains('Plus_Minus'), starts_with('A1_'), starts_with('A2_'), starts_with('iCF_'), starts_with('SH%_'), starts_with('GIVE_') & !contains('GAR'), starts_with('TAKE_') & !contains('GAR'),
-  #       starts_with('OZS_'), starts_with('NZS_'), starts_with('DZS_'), starts_with('ixG'))
-  select(faceoff_type, net_xg, Win_F1, Win_F2, Win_F3, Win_D1, Win_D2, Win_G1, Lose_F1, Lose_F2, Lose_F3, Lose_D1, Lose_D2, Lose_G1, starts_with('iCF_'))
-colnames(data)
-data = data %>%
-  filter(faceoff_type == 'Off zone won by Off team') %>%
-  select(-faceoff_type)
-cols = colnames(data %>% select(-net_xg))
-data <- data %>%
-  mutate(across(all_of(cols), ~scale(.))) %>%
-  mutate(across(all_of(cols), ~as.numeric(.)))
-data %>% write_csv('dataset_300k_rel_cols_no_na_processed_subset.csv') # used to be something like temp_train.csv
+  )) %>%
+  select(faceoff_type, event_zone, last_faceoff_winner, event_team, home_team, away_team, home_zone, where(is.numeric)) %>%
+  drop_na()
+
+model_name = ''
+
+prep_box_score_model = function(data) {
+  model_name = 'box_score'
+  data = data %>%
+    mutate(net_xg = winner_xg + loser_xg) %>%
+    mutate(faceoff_type = case_when(
+      event_zone != 'Neu' &  ((last_faceoff_winner == home_team & home_zone == 'Off') | (last_faceoff_winner != home_team & home_zone == 'Def')) ~ 'Off zone won by Off team',
+      event_zone != 'Neu' &  ((last_faceoff_winner != home_team & home_zone == 'Off') | (last_faceoff_winner == home_team & home_zone == 'Def')) ~ 'Def zone won by Def team',
+      event_zone != 'Neu' &  ((last_faceoff_winner == home_team & home_zone == 'Def') | (last_faceoff_winner != home_team & home_zone == 'Off')) ~ 'Def zone won by Def team',
+      event_zone != 'Neu' &  ((last_faceoff_winner != home_team & home_zone == 'Def') | (last_faceoff_winner == home_team & home_zone == 'Off')) ~ 'Off zone won by Off team',
+      TRUE ~ 'other'
+    ))
+  
+  data_off_off = data %>%
+    filter(faceoff_type == 'Off zone won by Off team') %>%
+    select(-faceoff_type)
+
+  data_def_def = data %>%
+    filter(faceoff_type == 'Def zone won by Def team') %>%
+    select(-faceoff_type)
+  
+  data_off_off = data_off_off %>%
+    select(net_xg, starts_with('G_') & !contains('_Plus_Minus'), starts_with('A1_'), starts_with('A2_'))
+  
+  data_def_def = data_def_def %>%
+    select(net_xg,starts_with('G_') & !contains('_Plus_Minus'), starts_with('A1_'), starts_with('A2_'))
+  
+  print(colnames(data_off_off))
+  print(colnames(data_def_def))
+  
+  cols = colnames(data_off_off %>% select(-net_xg))
+  data_off_off <- data_off_off %>%
+    mutate(across(all_of(cols), ~scale(.))) %>%
+    mutate(across(all_of(cols), ~as.numeric(.)))
+  
+  cols = colnames(data_def_def %>% select(-net_xg))
+  data_def_def <- data_def_def %>%
+    mutate(across(all_of(cols), ~scale(.))) %>%
+    mutate(across(all_of(cols), ~as.numeric(.)))
+  
+  print(nrow(data_off_off))
+  print(nrow(data_def_def))
+  
+  data_off_off %>% write_csv(paste0('training_data_', model_name, '_offensive_offensive.csv'))
+  data_def_def %>% write_csv(paste0('training_data_', model_name, '_defensive_defensive.csv'))
+}
+
+prep_expected_goals_model = function(data) {
+  model_name = 'expected_goals'
+  data = data %>%
+    mutate(net_xg = winner_xg + loser_xg) %>%
+    mutate(faceoff_type = case_when(
+      event_zone != 'Neu' &  ((last_faceoff_winner == home_team & home_zone == 'Off') | (last_faceoff_winner != home_team & home_zone == 'Def')) ~ 'Off zone won by Off team',
+      event_zone != 'Neu' &  ((last_faceoff_winner != home_team & home_zone == 'Off') | (last_faceoff_winner == home_team & home_zone == 'Def')) ~ 'Def zone won by Def team',
+      event_zone != 'Neu' &  ((last_faceoff_winner == home_team & home_zone == 'Def') | (last_faceoff_winner != home_team & home_zone == 'Off')) ~ 'Def zone won by Def team',
+      event_zone != 'Neu' &  ((last_faceoff_winner != home_team & home_zone == 'Def') | (last_faceoff_winner == home_team & home_zone == 'Off')) ~ 'Off zone won by Off team',
+      TRUE ~ 'other'
+    ))
+  
+  data_off_off = data %>%
+    filter(faceoff_type == 'Off zone won by Off team') %>%
+    select(-faceoff_type)
+  
+  data_def_def = data %>%
+    filter(faceoff_type == 'Def zone won by Def team') %>%
+    select(-faceoff_type)
+  
+  data_off_off = data_off_off %>%
+    select(net_xg, contains('xG') & !contains('team') & !contains('Plus_Minus') & !contains('RelTM') & !contains('winner_attributable_xg') & !contains('loser_attributable_xg') & !contains('winner_xg') & !contains('loser_xg') & !contains('xGF') & !contains('xGA'))
+  
+  data_def_def = data_def_def %>%
+    select(net_xg, contains('xG') & !contains('team') & !contains('Plus_Minus') & !contains('RelTM') & !contains('winner_attributable_xg') & !contains('loser_attributable_xg') & !contains('winner_xg') & !contains('loser_xg') & !contains('xGF') & !contains('xGA'))
+  
+  print(colnames(data_off_off))
+  print(colnames(data_def_def))
+  
+  cols = colnames(data_off_off %>% select(-net_xg))
+  data_off_off <- data_off_off %>%
+    mutate(across(all_of(cols), ~scale(.))) %>%
+    mutate(across(all_of(cols), ~as.numeric(.)))
+  
+  cols = colnames(data_def_def %>% select(-net_xg))
+  data_def_def <- data_def_def %>%
+    mutate(across(all_of(cols), ~scale(.))) %>%
+    mutate(across(all_of(cols), ~as.numeric(.)))
+  
+  print(nrow(data_off_off))
+  print(nrow(data_def_def))
+  
+  data_off_off %>% write_csv(paste0('training_data_', model_name, '_offensive_offensive.csv'))
+  data_def_def %>% write_csv(paste0('training_data_', model_name, '_defensive_defensive.csv'))
+}
+
+prep_relative_to_teammates_model = function(data) {
+  model_name = 'RelTM'
+  data = data %>%
+    mutate(net_xg = winner_xg + loser_xg) %>%
+    mutate(faceoff_type = case_when(
+      event_zone != 'Neu' &  ((last_faceoff_winner == home_team & home_zone == 'Off') | (last_faceoff_winner != home_team & home_zone == 'Def')) ~ 'Off zone won by Off team',
+      event_zone != 'Neu' &  ((last_faceoff_winner != home_team & home_zone == 'Off') | (last_faceoff_winner == home_team & home_zone == 'Def')) ~ 'Def zone won by Def team',
+      event_zone != 'Neu' &  ((last_faceoff_winner == home_team & home_zone == 'Def') | (last_faceoff_winner != home_team & home_zone == 'Off')) ~ 'Def zone won by Def team',
+      event_zone != 'Neu' &  ((last_faceoff_winner != home_team & home_zone == 'Def') | (last_faceoff_winner == home_team & home_zone == 'Off')) ~ 'Off zone won by Off team',
+      TRUE ~ 'other'
+    ))
+  
+  data_off_off = data %>%
+    filter(faceoff_type == 'Off zone won by Off team') %>%
+    select(-faceoff_type)
+  
+  data_def_def = data %>%
+    filter(faceoff_type == 'Def zone won by Def team') %>%
+    select(-faceoff_type)
+  
+  data_off_off = data_off_off %>%
+    select(net_xg, contains('RelTM'))
+  
+  data_def_def = data_def_def %>%
+    select(net_xg, contains('RelTM'))
+  
+  print(colnames(data_off_off))
+  print(colnames(data_def_def))
+  
+  cols = colnames(data_off_off %>% select(-net_xg))
+  data_off_off <- data_off_off %>%
+    mutate(across(all_of(cols), ~scale(.))) %>%
+    mutate(across(all_of(cols), ~as.numeric(.)))
+  
+  cols = colnames(data_def_def %>% select(-net_xg))
+  data_def_def <- data_def_def %>%
+    mutate(across(all_of(cols), ~scale(.))) %>%
+    mutate(across(all_of(cols), ~as.numeric(.)))
+  
+  print(nrow(data_off_off))
+  print(nrow(data_def_def))
+  
+  data_off_off %>% write_csv(paste0('training_data_', model_name, '_offensive_offensive.csv'))
+  data_def_def %>% write_csv(paste0('training_data_', model_name, '_defensive_defensive.csv'))
+}
+
+prep_zone_starts_model = function(data) {
+  model_name = 'starts'
+  data = data %>%
+    mutate(net_xg = winner_xg + loser_xg) %>%
+    mutate(faceoff_type = case_when(
+      event_zone != 'Neu' &  ((last_faceoff_winner == home_team & home_zone == 'Off') | (last_faceoff_winner != home_team & home_zone == 'Def')) ~ 'Off zone won by Off team',
+      event_zone != 'Neu' &  ((last_faceoff_winner != home_team & home_zone == 'Off') | (last_faceoff_winner == home_team & home_zone == 'Def')) ~ 'Def zone won by Def team',
+      event_zone != 'Neu' &  ((last_faceoff_winner == home_team & home_zone == 'Def') | (last_faceoff_winner != home_team & home_zone == 'Off')) ~ 'Def zone won by Def team',
+      event_zone != 'Neu' &  ((last_faceoff_winner != home_team & home_zone == 'Def') | (last_faceoff_winner == home_team & home_zone == 'Off')) ~ 'Off zone won by Off team',
+      TRUE ~ 'other'
+    ))
+  
+  data_off_off = data %>%
+    filter(faceoff_type == 'Off zone won by Off team') %>%
+    select(-faceoff_type)
+  
+  data_def_def = data %>%
+    filter(faceoff_type == 'Def zone won by Def team') %>%
+    select(-faceoff_type)
+  
+  data_off_off = data_off_off %>%
+    select(net_xg, contains('OZS'), contains('NZS'), contains('DZS'))
+  
+  data_def_def = data_def_def %>%
+    select(net_xg, contains('OZS'), contains('NZS'), contains('DZS'))
+  
+  print(colnames(data_off_off))
+  print(colnames(data_def_def))
+  
+  cols = colnames(data_off_off %>% select(-net_xg))
+  data_off_off <- data_off_off %>%
+    mutate(across(all_of(cols), ~scale(.))) %>%
+    mutate(across(all_of(cols), ~as.numeric(.)))
+  
+  cols = colnames(data_def_def %>% select(-net_xg))
+  data_def_def <- data_def_def %>%
+    mutate(across(all_of(cols), ~scale(.))) %>%
+    mutate(across(all_of(cols), ~as.numeric(.)))
+  
+  print(nrow(data_off_off))
+  print(nrow(data_def_def))
+  
+  data_off_off %>% write_csv(paste0('training_data_', model_name, '_offensive_offensive.csv'))
+  data_def_def %>% write_csv(paste0('training_data_', model_name, '_defensive_defensive.csv'))
+}
+
+prep_box_score_model(data)
+prep_expected_goals_model(data)
+prep_relative_to_teammates_model(data)
+prep_zone_starts_model(data)
