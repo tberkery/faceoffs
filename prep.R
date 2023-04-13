ebd0896f7d348f987dc3cd5f34496cb17d51da1f
tberkery <78045727+tberkery@users.noreply.github.com>
2023-04-13 14:42:21 -0400

    minor changes to files loaded in run.R

--- prep.R
+++ prep.R
@@ -11,9 +11,10 @@ data = data %>%
     TRUE ~ 'other'
   ))
 data = data %>%
-  select(faceoff_type, net_xg, Win_F1, Win_F2, Win_F3, Win_D1, Win_D2, Win_G1, Lose_F1, Lose_F2, Lose_F3, Lose_D1, Lose_D2, Lose_G1,
-         starts_with('G_') & !contains('Plus_Minus'), starts_with('A1_'), starts_with('A2_'), starts_with('iCF_'), starts_with('SH%_'), starts_with('GIVE_') & !contains('GAR'), starts_with('TAKE_') & !contains('GAR'),
-         starts_with('OZS_'), starts_with('NZS_'), starts_with('DZS_'), starts_with('ixG'))
+  #select(faceoff_type, net_xg, Win_F1, Win_F2, Win_F3, Win_D1, Win_D2, Win_G1, Lose_F1, Lose_F2, Lose_F3, Lose_D1, Lose_D2, Lose_G1,
+  #       starts_with('G_') & !contains('Plus_Minus'), starts_with('A1_'), starts_with('A2_'), starts_with('iCF_'), starts_with('SH%_'), starts_with('GIVE_') & !contains('GAR'), starts_with('TAKE_') & !contains('GAR'),
+  #       starts_with('OZS_'), starts_with('NZS_'), starts_with('DZS_'), starts_with('ixG'))
+  select(faceoff_type, net_xg, Win_F1, Win_F2, Win_F3, Win_D1, Win_D2, Win_G1, Lose_F1, Lose_F2, Lose_F3, Lose_D1, Lose_D2, Lose_G1, starts_with('iCF_'))
 colnames(data)
 data = data %>%
   filter(faceoff_type == 'Off zone won by Off team') %>%
@@ -22,4 +23,4 @@ cols = colnames(data %>% select(-net_xg))
 data <- data %>%
   mutate(across(all_of(cols), ~scale(.))) %>%
   mutate(across(all_of(cols), ~as.numeric(.)))
-data %>% write_csv('dataset_300k_rel_cols_no_na_processed.csv') # used to be something like temp_train.csv
+data %>% write_csv('dataset_300k_rel_cols_no_na_processed_subset.csv') # used to be something like temp_train.csv
