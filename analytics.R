800a74849607401f5fa0f9e62571227c8128c3d2
tberkery <78045727+tberkery@users.noreply.github.com>
2023-04-24 18:14:39 -0400

    added a few more lines of code from console to analytics.R

--- analytics.R
+++ analytics.R
@@ -11,3 +11,12 @@ ggplot(off_off, aes(x = TOI_Win_All, y = net_xg, group = GAR_Win_All)) + geom_de
 geom_smooth(method = 'lm')
 ggplot(off_off, aes(x = net_xg)) + geom_histogram(bins = 100)
 ggplot(off_off, aes(x = GAR_Win_All, y = net_xg)) + geom_density(kernel = 'gaussian')
+
+
+temp2 = pbp %>% select(game_id, event_type) %>% filter(event_type == 'FAC') %>% group_by(game_id) %>% mutate(event_type = n()) %>% distinct(.keep_all = TRUE) %>% arrange(desc(event_type))
+temp = pbp %>% select(event_type) %>% filter(event_type == 'FAC')
+temp = pbp %>% select(event_type, event_zone) %>% filter(event_type == 'FAC')
+temp = pbp %>% select(event_type, event_zone) %>% filter(event_type == 'FAC') %>% group_by(event_zone) %>% mutate(event_type = n()) %>% distinct(.keep_all = TRUE)
+View(temp)
+(98686+98823) / (98686+98823+91670)
+off / tot * 0.023 + def / tot * 0.020
