2ddf024a726300307650030c8d8b17dfb811f411
tberkery <78045727+tberkery@users.noreply.github.com>
2023-04-24 19:06:27 -0400

    minor changes to analytics.R

--- analytics.R
+++ analytics.R
@@ -5,7 +5,7 @@ off_off = read_csv("training_data_all_offensive_offensive.csv")
 def_def = read_csv("training_data_all_defensive_defensive.csv")
 
 summary(off_off$net_xg)
-
+summary(def_def$net_xg)
 ggplot(off_off, aes(x = TOI_Win_All, y = net_xg, group = GAR_Win_All)) + geom_density(kernel = 'gaussian')
 
 geom_smooth(method = 'lm')
