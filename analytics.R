580b7eba9fbc13434a8b44a4e36c4c40f1f0f503
tberkery <78045727+tberkery@users.noreply.github.com>
2023-05-11 14:33:49 -0400

    added more calculations to analytics.R among other changes.

--- analytics.R
+++ analytics.R
@@ -2,9 +2,10 @@ library(tidyverse)
 library(ggplot2)
 
 games_lost_by_faceoffs_2023 = function() {
-  df = read_csv("EH_pbp_query_20222023_2023-05-03.csv")
+  df = read_csv("EH_pbp_query_20222023_2023-05-11.csv")
   df_new = df %>%
     select(game_id, game_date, game_period, home_team, away_team, game_strength_state, game_score_state, game_seconds, event_description, event_team, event_type, event_player_1, event_player_2, event_player_3) %>%
+    filter(event_type == 'FAC' | event_type == 'GOAL') %>%
     filter(game_period >= 3) %>%
     mutate(last_faceoff_time = lag(game_seconds)) %>%
     mutate(time_since_faceoff = game_seconds - last_faceoff_time) %>%
@@ -13,6 +14,37 @@ games_lost_by_faceoffs_2023 = function() {
   
   leafs_bolts = df_new %>%
     filter(home_team == 'TOR' | away_team == 'TOR')
+  
+  within_10_secs = df_new %>% filter(time_since_faceoff <= 10)
+  within_10_secs_grouped = within_10_secs %>% select(event_team) %>% group_by(event_team) %>% mutate(count = n()) %>% distinct(event_team, .keep_all = TRUE) %>% arrange(desc(count))
+
+  df_new_all = df %>%
+    select(game_id, game_date, game_period, home_team, away_team, game_strength_state, game_score_state, game_seconds, event_description, event_team, event_type, event_player_1, event_player_2, event_player_3) %>%
+    filter(event_type == 'FAC' | event_type == 'GOAL') %>%
+    mutate(last_faceoff_time = lag(game_seconds)) %>%
+    mutate(time_since_faceoff = game_seconds - last_faceoff_time) %>%
+    filter(event_type == 'GOAL') %>%
+    arrange(time_since_faceoff, desc(game_seconds))
+  
+  leafs_bolts = df_new %>%
+    filter(home_team == 'TOR' | away_team == 'TOR')
+  
+  within_10_secs_all = df_new_all %>% filter(time_since_faceoff <= 10)
+  within_10_secs_grouped_all = within_10_secs_all %>% select(event_team) %>% group_by(event_team) %>% mutate(count = n()) %>% distinct(event_team, .keep_all = TRUE) %>% arrange(desc(count))
+
+  df_new_same_zone = df %>%
+    select(game_id, game_date, game_period, home_team, away_team, game_strength_state, game_score_state, game_seconds, event_description, event_team, event_type, event_zone, event_player_1, event_player_2, event_player_3) %>%
+    filter(event_type == 'FAC' | event_type == 'GOAL') %>%
+    mutate(last_faceoff_time = lag(game_seconds)) %>%
+    mutate(last_faceoff_same_zone = ifelse((lag(event_zone) == event_zone & lag(event_team) == event_team) | (lag(event_zone) != event_zone & lag(event_team) != event_team), TRUE, FALSE)) %>%
+    mutate(time_since_faceoff = game_seconds - last_faceoff_time) %>%
+    filter(event_type == 'GOAL') %>%
+    arrange(time_since_faceoff, desc(game_seconds))
+
+  within_10_secs_all = df_new_all %>% filter(time_since_faceoff <= 10)
+  within_10_secs_grouped_all = within_10_secs_all %>% select(event_team) %>% group_by(event_team) %>% mutate(count = n()) %>% distinct(event_team, .keep_all = TRUE) %>% arrange(desc(count))
+
+  tor_fla = df_new %>% filter((home_team == 'TOR' & away_team == 'FLA') | (home_team == 'FLA' & away_team == 'TOR'))
 }
 
 
