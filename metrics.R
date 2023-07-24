caa337c6678d714970134b0333a7c4f535bdda6a
tberkery <78045727+tberkery@users.noreply.github.com>
2023-07-23 21:02:24 -0400

    tinkering but calling it a night. this code is in an incomplete state

--- metrics.R
+++ metrics.R
@@ -19,17 +19,27 @@ identify_situation = function(df) {
 get_next_faceoff_location = function(df) {
   df_fac = df %>%
     filter(event_type == 'FAC') %>%
-    mutate(next_faceoff_location = lead(event_zone, 1))
+    mutate(next_faceoff_location = lead(home_zone, 1)) %>%
+    mutate(next_event_team = lead(event_team, 1)) %>%
+    mutate(next_faceoff_location = case_when(
+      next_faceoff_location == "Neu" ~ next_faceoff_location,
+      event_team == home_team & next_event_team == home_team ~ next_faceoff_location,
+      event_team == away_team & next_event_team == away_team ~ if(next_faceoff_location == "Off", "Def", "Off")
+      event_team == home_team & next_event_team == away_team ~ 
+    ))
+    filter(game_id == lead(game_id, 1), game_date == lead(game_date, 1))
   df_updated = df %>%
-    left_join(df_fac, by = c('season', 'game_id', 'event_index',
-                             'game_period', 'game_seconds', 'event_type'),
+    left_join(df_fac, by = c('season', 'game_id',
+                             'game_period', 'event_type'),
               suffix = c("", "_ignore")) %>%
     select(-ends_with("_ignore"))
+  quick_look = df_updated %>% 
+    ungroup() %>% 
+    filter(event_type == "FAC") %>% 
+    select(next_faceoff_location) %>% 
+    drop_na()
   print(paste0("Before joining for next faceoff location, there are ", nrow(df_fac), " faceoffs."))
-  print(paste0("After joining for next faceoff location, there are ", nrow(df_updated 
-                                                                           %>% filter(event_type == "FAC") 
-                                                                           %>% drop_na(next_faceoff_location)
-                                                                           )))
+  print(paste0("After joining for next faceoff location, there are ", nrow(quick_look)))
   return(df_updated)
 }
 # Percentage of favorable zone time before next faceoff
