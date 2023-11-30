366a6eeafac2d4e90c65c76dc6d624377e11f59f
tberkery <78045727+tberkery@users.noreply.github.com>
2023-11-29 21:58:19 -0500

    exploring IZT implied xG

--- translate.R
+++ translate.R
@@ -28,10 +28,56 @@ bj_c = bj_contextualized %>%
   mutate(last_faceoff_gs = zoo::na.locf(interim, fromLast = FALSE, na.rm = FALSE)) %>%
   mutate(last_faceoff_gs = as.numeric(last_faceoff_gs)) %>%
   mutate(interim = dplyr::case_when(
-    event_type == "FAC" ~ as.character(FA_zone_time),
+    event_type == "FAC" ~ as.character(game_seconds + FA_zone_time),
     TRUE ~ as.character(NA_real_)
   )) %>%
   mutate(last_faceoff_faz = zoo::na.locf(interim, fromLast = FALSE, na.rm = FALSE)) %>%
   mutate(last_faceoff_faz = as.numeric(last_faceoff_faz)) %>%
   select(-interim)
 
+bj = bj_c %>%
+  mutate(FA_xG_status = ifelse(game_seconds >= last_faceoff_gs & game_seconds <= last_faceoff_faz, TRUE, FALSE)) %>%
+  filter(last_faceoff_gs >= 0,
+         last_faceoff_faz >= 0,
+         game_seconds >= 0)
+
+summary_stats = bj %>%
+  arrange(season, game_id, game_seconds) %>%
+  mutate(ZT_since_last_event = case_when(
+    event_type == "FAC" ~ 0,
+    game_seconds >= lag(game_seconds) ~ game_seconds - lag(game_seconds),
+    TRUE ~ NA_real_,
+  )) %>%
+  filter(FA_xG_status == TRUE) %>%
+  group_by(game_id, season, game_strength_state, event_zone, last_faceoff_gs, FA_xG_status) %>%
+  summarize(cumulative_xG = sum(pred_goal, na.rm = TRUE),
+            cumulative_ZT = sum(ZT_since_last_event, na.rm = TRUE),
+            .groups = 'keep')
+
+summary_figures = bj %>%
+  arrange(season, game_id, game_seconds) %>%
+  mutate(ZT_since_last_event = case_when(
+    event_type == "FAC" ~ 0,
+    game_seconds >= lag(game_seconds) ~ game_seconds - lag(game_seconds),
+    TRUE ~ NA_real_,
+  )) %>%
+  filter(FA_xG_status == TRUE) %>%
+  filter(event_zone %in% c("Off", "Neu", "Def")) %>%
+  group_by(game_strength_state, event_zone) %>%
+  summarize(cumulative_xG = sum(pred_goal, na.rm = TRUE),
+            cumulative_ZT = sum(ZT_since_last_event, na.rm = TRUE),
+            xG_per_ZT = sum(pred_goal, na.rm = TRUE) / sum(ZT_since_last_event, na.rm = TRUE),
+            .groups = 'keep')
+
+five_on_five = summary_stats %>%
+  filter(game_strength_state == "5v5")
+
+five_on_four = summary_stats %>%
+  filter(game_strength_state == "5v4")
+
+four_on_five = summary_stats %>%
+  filter(game_strength_state == "4v5")
+
+print(sum(five_on_five$cumulative_xG, na.rm = TRUE) / sum(five_on_five$cumulative_ZT, na.rm = TRUE))
+print(sum(five_on_four$cumulative_xG, na.rm = TRUE) / sum(five_on_four$cumulative_ZT, na.rm = TRUE))
+print(sum(four_on_five$cumulative_xG, na.rm = TRUE) / sum(four_on_five$cumulative_ZT, na.rm = TRUE))
